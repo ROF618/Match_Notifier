50709d1b03210108503041dcecb1f5c4a37f041b
Chris <surferbum618@ymail.com>
2020-03-31 02:09:10 -0500

    pushing finished and working version, still needs to be optimized

---
+++ test_client.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest import IsolatedAsyncioTestCase
+import client
+
+events = []
+
+
+class TestClient(unittest.TestCase):
+
+	def test_time_getter(self):
+		result = client.time_getter('')
+		self.assert
+
+class TestResponses(IsolatedAsyncioTestCase):
+	
+	def setUp():
+		event.append("setUP")
+
+
+if __name__ == '__main__':
+	unittest.main()
